2b6480192ec1315ce1da50a4ceab6943f0ccc501
AGoldian <goldianmail@yandex.ru>
2023-03-12 23:51:17 +0500

    refactor

--- trash_smell/duplicates.py
+++ trash_smell/duplicates.py
@@ -1,21 +1,29 @@
 import glob
 import hashlib
+import imagehash
+from PIL import Image
+import pandas as pd
+from tqdm import tqdm
 
-hash_dict = dict()
-for file in glob.glob(r"D:\Kaggle\CleanCodeCup\2023\2023\images\images\*.jpg"):
+hash_size = 32
+
+filename = []
+md5_hash = []
+avg_hash = []
+
+for file in tqdm(glob.glob(r"W:\Users\Goldian\Desktop\data\images\images\*.jpg")):
+    file_name = file.split("\\")[-1]
+
     with open(file, 'rb') as f:
         file_hash = hashlib.md5(f.read()).hexdigest()
-        file_name = file.split("\\")[-1]
-        hash_dict[file_name] = file_hash
-import csv
+        
+    with Image.open(file) as img:
+        temp_hash = imagehash.average_hash(img, hash_size)
 
-# Open a file for writing
-with open('file_hashes.csv', 'w', newline='') as csvfile:
-    writer = csv.writer(csvfile)
 
-    # Write the header row
-    writer.writerow(['Filename', 'MD5_Hash'])
+    filename.append(file_name)
+    md5_hash.append(file_hash)
+    avg_hash.append(temp_hash)
 
-    # Write each row of data
-    for file_name, md5_hash in hash_dict.items():
-        writer.writerow([file_name, md5_hash])
+
+pd.DataFrame({'image': filename, 'md5': md5_hash, 'avg': avg_hash}).to_csv('beda.csv', index=False)
