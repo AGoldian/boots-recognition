dbfdd5c3abc143b1abaf5a9fad7fad197fd34ea8
AGoldian <goldianmail@yandex.ru>
2023-03-12 18:38:52 +0500

    clustering on csv

--- clustering.py
+++ clustering.py
@@ -3,9 +3,10 @@ import os
 import matplotlib.pyplot as plt
 import cv2
 import numpy as np
-from sklearn.cluster import KMeans
+from sklearn.cluster import KMeans, DBSCAN
 
-ROOT_DIR = r'data/images/'
+ROOT_DIR = r'W:\Users\Goldian\Desktop\data\images\images\\'
+TRAIN_CSV = r'hash\clean_train.csv'
 
 def read_image(root_dir):
     ids = []
@@ -15,7 +16,7 @@ def read_image(root_dir):
         if filename.endswith('.jpg'):
             img = cv2.imread(root_dir + filename)
 
-            if img != None:
+            if type(img) != None:
                 img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
                 img = cv2.resize(img, dsize=(128, 128))
                 data.append(img)
@@ -25,9 +26,29 @@ def read_image(root_dir):
 
     return np.array(data), ids
 
+def read_data(root_dir=ROOT_DIR, csv=TRAIN_CSV):
+    files = pd.read_csv(TRAIN_CSV)['image'].values
+    ids = []
+    data = []
 
-def divide_clusters(root_dir=ROOT_DIR, num_clusters=2, mode='cpu'):
-    dataframe, ids = read_image(root_dir)
+    for filename in files:
+        print(filename)
+        img = cv2.imread(root_dir + filename)
+ 
+        if img != None:
+            img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+            img = cv2.resize(img, dsize=(128, 128))
+            data.append(img)
+            ids.append(root_dir + filename)
+        else:
+            print(f'Error {filename}')
+
+    return np.array(data), ids
+
+
+
+def divide_clusters_kmeans(root_dir=ROOT_DIR, num_clusters=2, mode='cpu'):
+    dataframe, ids = read_data(root_dir)
     dataframe = dataframe.reshape(-1, 128*128)
     kmean = KMeans(n_clusters=num_clusters)
     cluster = kmean.fit_predict(dataframe)
@@ -35,6 +56,20 @@ def divide_clusters(root_dir=ROOT_DIR, num_clusters=2, mode='cpu'):
     output = pd.DataFrame({'id': ids, 'cluster': cluster})
 
     return output
+
+
+def divide_clusters_DBSCAN(root_dir=ROOT_DIR):
+    dataframe, ids = read_data(root_dir)
+    dataframe_res = dataframe.reshape(-1, 128*128)
+    bedolaga = DBSCAN(min_samples=2, n_jobs=-1)
+
+    cluster = bedolaga.fit_predict(dataframe_res)
+
+    output = pd.DataFrame({'id': ids, 'cluster': cluster})
+
+    return output
+
+
 
 
 def plot_images(dataframe, cluster, num_images=10):
@@ -62,5 +97,5 @@ def plot_images(dataframe, cluster, num_images=10):
 # from libKMCUDA import kmeans_cuda
 
 if __name__ == '__main__':
-    df = divide_clusters()
+    df = divide_clusters_DBSCAN()
     plot_images(df, cluster=1)
